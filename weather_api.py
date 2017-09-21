2f3d7f18abe95df158f2471b7b0dc4956b3b4b0b
Sai Pathuri <saipathuri@gmail.com>
2017-09-20 19:10:27 -0500

    remove custom city

--- weather_api.py
+++ weather_api.py
@@ -3,7 +3,7 @@ import requests
 city_name = raw_input('Choose a city: ')
 print 'You picked ' + city_name
 
-endpoint = "https://publicdata-weather.firebaseio.com/%s/currently.json"%city_name
+endpoint = "https://publicdata-weather.firebaseio.com/dallas/currently.json"
 response = requests.get(endpoint)
 data = response.json()
 
